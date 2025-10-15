562bbc7c602ca5231f4582046fba0c3b6d435110
qsttt <125194059+qsttt@users.noreply.github.com>
2025-10-15 11:49:21 +0500

    Delete bot directory

--- bot/keyboards/common.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def confirm_kb():
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="Повторить", callback_data="again")],
-            [InlineKeyboardButton(text="Отменить последний", callback_data="undo")]
-        ]
-    )
